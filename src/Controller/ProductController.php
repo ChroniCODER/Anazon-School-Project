cb6ad93b4a1525fdc6ddae79032b0b0dc2c7ffb1
ChroniCODER <thomas.leherisse@gmail.com>
2023-01-27 15:47:37 +0100

    ajout conditions sur productController et detail.html.twi pour empecher ajout d'un com si com deja posté pour ce produit par user connecté

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,30 +5,54 @@ namespace App\Controller;
 use App\Entity\Review;
 use App\Form\ReviewFormType;
 use App\Repository\ProductRepository;
+use App\Repository\ReviewRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProductController extends AbstractController
 {
     #[Route('/products/{id}', name: 'app_product')]
-    public function detail(ProductRepository $productRepository, string $id): Response
+    public function detail(ProductRepository $productRepository, string $id, EntityManagerInterface $em, Request $request, ReviewRepository $reviewRepository): Response
     {
+
         $review = new Review();
+        $product = $productRepository->findOneById($id);
 
         $form = $this->createForm(ReviewFormType::class, $review);
         $form->get('user')->setData($this->getUser());
-        
 
-        $product = $productRepository->findOneById($id);
         $form->get('product')->setData($product);
         $category = $product->getCategory();
+        $review->setProduct($product);
+        $review->setUser($this->getUser());
 
-                
-        return $this->render('product/detail.html.twig', [
-            'category' => $category,
-            'product' => $productRepository->findOneById($id),
-            'review_form' => $form->createView(),
-        ]);
+        $existingReview = $reviewRepository->findOneBy(['product' => $product, 'user' => $this->getUser()]);
+        if ($existingReview) {
+            // l'utilisateur a déjà déposé une critique pour ce produit
+            return $this->render('product/detail.html.twig', [
+                'category' => $category,
+                'existingReview' => $existingReview,
+                'product' => $productRepository->findOneById($id),
+            ]);
+        } else {
+
+
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->persist($review);
+                $em->flush();
+                $this->addFlash('success', 'Votre review a été ajoutée avec succès');
+                return $this->redirectToRoute('app_product', ['id' => $product->getId()]);
+            }
+
+            return $this->render('product/detail.html.twig', [
+                'category' => $category,
+                'product' => $productRepository->findOneById($id),
+                'review_form' => $form->createView(),
+            ]);
+        }
     }
 }
